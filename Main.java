f1ab2fa6a413dcaaae583b76dd0db4e875c40ccf
liuwanzhen <liuwanzhenlfuwenjun@gmail.com>
2012-07-22 21:05:07 +0800

    push test

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+// git pubsh test
 		System.out.println( 1 <<0x01f);
 		System.out.println(Integer.bitCount(0));
 		
